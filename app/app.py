b94a3b7fd6276e47e3d9250cedd36d56d4648ca1
Raoul Collenteur <raoulcollenteur@gmail.com>
2023-09-05 15:43:44 +0200

    Update layout

--- app/app.py
+++ app/app.py
@@ -5,42 +5,58 @@ import os
 import base64
 from folium import IFrame
 import numpy as np
+from bokeh.resources import INLINE
 
 
 pn.extension(sizing_mode="stretch_width")
 
-m = folium.Map(zoom_start=12, max_bounds=True)
-
-folium_pane = pn.pane.plot.Folium(m, height=500)
+m = folium.Map(zoom_start=12)
+folium_pane = pn.pane.plot.Folium(m, height=500, sizing_mode="stretch_width")
 
 dir = "./img/"
 for fl in os.listdir(dir):
     if fl.endswith(".png"):
         idx = str(fl.split(".")[0])
         encoded = base64.b64encode(open("./img/{}.png".format(idx), "rb").read())
-        html = '<img src="data:image/png;base64,{}">'.format
-        iframe = IFrame(html(encoded.decode("UTF-8")), width=630, height=380)
-        popup = folium.Popup(iframe, max_width=1000)
+        html = '<img src="data:image/png;base64,{}" width="500px">'.format
+        iframe = IFrame(html(encoded.decode("UTF-8")), width=500, height=380)
+        popup = folium.Popup(iframe, max_width=500)
 
         folium.Marker(
             location=[50 + np.random.rand(1) * 10, 10 + np.random.rand(1) * 100],
             popup=popup,
             icon=folium.Icon(),
+            color="#009BE0",
         ).add_to(m)
     else:
         continue
 m.add_child(folium.LatLngPopup())
 
+text = """
+Welcome to the Pastas model demo website, showing the locations of the Pastas models
+around the world. Click on the markers to see the Pastas model and more information on 
+the Pastas application. 
+
+Want to add a model? Send a pas-file of a models you want to add to us by submitting an 
+issue on <a href="https://github.com/pastas/worldtour/issues">Github</a>.
+"""
+
+left = pn.Column(
+    "# Welcome!",
+    pn.layout.Divider(),
+    text,
+    styles=dict(background="whitesmoke"),
+)
+
+png_pane = pn.pane.PNG("app/img/logo_small.png")
 
 template = pn.template.BootstrapTemplate(
     title="Pastas around the world",
-    sidebar=[],
+    sidebar=[left, png_pane],
+    main_max_width="100%",
+    main=[folium_pane],
+    header_background="#009BE0",
 )
-# Append a layout to the main area, to demonstrate the list-like API
-template.main.append(pn.Row(folium_pane))
 
 # template.servable()
-
-from bokeh.resources import INLINE
-
 template.save("index.html", resources=INLINE)
